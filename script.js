b15acd270f1ea90d5c06b2c209f2a3ac6b29f3a9
William Wassmann <wmwassmann@hotmail.com>
2020-07-12 21:52:15 -0700

    Feeling MUCH better with this assignment than I did with the last. This iteration is a proof of concept - tonight and tomorrow will be working to get the local storage functions prepared.

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+// create a field with html to store user input in the local storage
+    // this input should have fields for the time, the place, and a note for what the appointment is about.
+        // In progress
+// create a button to save this input into the local storage
+    // Button complete, need to code it
+
+// create a button to delete selected section (bonus)
+    // might add this, finishing assignment first
+
+// create a reader of the current time and day.
+    // Have a rudimentary idea of how to do this. 
+
+// code the time to correspond to a color.
+    // Need to research this one. I assume it's just an if/then statement that call on functions to switch the appearance. 
